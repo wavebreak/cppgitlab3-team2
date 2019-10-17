721993d8c247b18f2a004219d08f2f602633bb71
lijophilip <lijo.philip@tulsacc.edu>
2019-10-17 10:34:57 -0500

    ModifyItem Function

--- ShoppingCart.cpp
+++ ShoppingCart.cpp
@@ -33,7 +33,76 @@ void ShoppingCart::RemoveItem(string name) {
 
 void ShoppingCart::ModifyItem(ItemToPurchase item) {
 
-}
+        bool found = false;
+
+        //Traverse the vector cartItems using a
+
+        //for loop.
+
+        for (vector<ItemToPurchase>::iterator itemList = cartItems.begin(); itemList != cartItems.end(); itemList++)
+
+        {
+            //If the current name of the list
+
+            //matched with the required name.
+
+            if ((*itemList).GetName() == item.GetName())
+
+            {
+
+                //If the name is found, then make
+
+                //the boolean variable true.
+
+                found = true;
+
+                //Store the reference of the pointer
+
+                //to object it to the item.
+
+                ItemToPurchase& item = *itemList;
+
+                //If the description is none, then
+
+                //set the description.
+
+                if (item.GetDescription() != "none")
+                    item.SetDescription(item.GetDescription());
+
+                //If the price is 0, then set the price
+
+                //of the item.
+
+                if (item.GetPrice() != 0)
+                    item.SetPrice(item.GetPrice());
+
+                //If the quantity is 0, then set the
+
+                //quantity.
+
+                if (item.GetQuantity() != 0)
+                    item.SetQuantity(item.GetQuantity());
+
+            }
+
+        }
+
+        //If the value of the variable found is not true,
+
+        //then display an appropriate message.
+
+        if (found != true)
+
+        {
+
+            cout << "Item not found in cart. ";
+
+            cout << "Nothing modified." << endl;
+
+        }
+
+    }
+
 
 int ShoppingCart::GetNumItemsInCart() {
     int size = 0;
